8a4abaf2e21a7342d37e13197de59bea5465b08d
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-21 23:24:55 +0900

    Use Jetpack Compose in Android

--- androidApp/src/main/java/com/example/compose_playground/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/compose_playground/android/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.compose_playground.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.compose_playground.Greeting
-import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 fun greet(): String {
     return Greeting().greeting()
@@ -12,9 +20,24 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            App()
+        }
+    }
+}
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+@Composable
+fun App() {
+    MaterialTheme {
+        Surface {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = greet()
+                )
+            }
+        }
     }
 }
