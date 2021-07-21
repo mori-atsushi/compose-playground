2c07420a49682984292cc45d046055afdd352721
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-21 22:51:26 +0900

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ComposePlayground"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/compose_playground/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.compose_playground
+
+expect class Platform() {
+    val platform: String
+}
