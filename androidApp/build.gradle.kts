359b7be90ede0d312ab8f34c81737e1e49ca60d4
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-21 23:13:19 +0900

    Add compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,25 +3,48 @@ plugins {
     kotlin("android")
 }
 
-dependencies {
-    implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-}
-
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     defaultConfig {
         applicationId = "com.example.compose_playground.android"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk = 21
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-rc02"
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.4.0")
+
+    // androidx
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+
+    // compose
+    implementation("androidx.compose.ui:ui:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.foundation:foundation:1.0.0-rc02")
+    implementation("androidx.compose.material:material:1.0.0-rc02")
 }
