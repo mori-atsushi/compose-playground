ae60a71bcf44aa4a384868d9dfddce0f5d77d92c
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-22 21:54:23 +0900

    Fix ios project

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -49,3 +49,18 @@ android {
         targetSdkVersion(30)
     }
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
+    val targetDir = File(buildDir, "xcode-frameworks")
+
+    group = "build"
+    dependsOn(framework.linkTask)
+    inputs.property("mode", mode)
+
+    from({ framework.outputDirectory })
+    into(targetDir)
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
