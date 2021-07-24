4c449049de6a230bf6b15f9a40a64df2e6a7a382
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-24 20:58:30 +0900

    Implement to parse json

--- common/shared/src/commonMain/kotlin/com/example/compose_playground/network/Sample.kt
+++ common/shared/src/commonMain/kotlin/com/example/compose_playground/network/Sample.kt
@@ -1,24 +1,28 @@
 package com.example.compose_playground.network
 
+import com.example.compose_playground.model.User
 import com.example.compose_playground.util.log.Log
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
 object Sample {
     fun sample() {
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
         val client = HttpClient {
             install(JsonFeature) {
-                serializer = KotlinxSerializer()
+                serializer = KotlinxSerializer(json)
             }
         }
         GlobalScope.launch {
-            val response: HttpResponse = client.get("https://api.github.com/users/google")
-            Log.d("Sample", response.status)
+            val response: User = client.get("https://api.github.com/users/google")
+            Log.d("Sample", response)
         }
     }
 }
