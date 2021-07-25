de8b58153d9e00100e69f4e1cf08ee11ca99b860
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-25 20:54:55 +0900

    Implement TopViewModel for input

--- common/shared/src/commonMain/kotlin/com/example/compose_playground/viewmodel/TopViewModel.kt
+++ common/shared/src/commonMain/kotlin/com/example/compose_playground/viewmodel/TopViewModel.kt
@@ -3,8 +3,8 @@ package com.example.compose_playground.viewmodel
 import com.example.compose_playground.model.Repo
 import com.example.compose_playground.network.GitHubApi
 import com.example.compose_playground.util.log.Log
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class TopViewModel(
@@ -17,20 +17,57 @@ class TopViewModel(
     private val _output = MutableStateFlow(Output())
     val output: StateFlow<Output> = _output
 
+    private val submitEvent = MutableSharedFlow<Unit>(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+
     init {
-        viewModelScope.launch {
-            try {
-                val repoList = gitHubApi.getRepoList("Google")
-                _output.value = Output(
-                    repoList = repoList
-                )
-            } catch (e: Throwable) {
-                Log.e(TAG, e)
+        submitEvent
+            .onEach {
+                try {
+                    val userName = output.value.userName
+                    val repoList = gitHubApi.getRepoList(userName)
+                    _output.value = _output.value.copy(
+                        repoList = repoList
+                    )
+                } catch (e: Throwable) {
+                    Log.e(TAG, e)
+                }
             }
+            .launchIn(viewModelScope)
+
+        viewModelScope.launch {
+            submitEvent.emit(Unit)
         }
     }
 
+    fun input(input: Input) {
+        when (input) {
+            is Input.Submit -> submit()
+            is Input.SetUserName -> setUserName(input)
+        }
+    }
+
+    private fun submit() {
+        viewModelScope.launch {
+            submitEvent.emit(Unit)
+        }
+    }
+
+    private fun setUserName(input: Input.SetUserName) {
+        _output.value = _output.value.copy(
+            userName = input.userName
+        )
+    }
+
     data class Output(
+        val userName: String = "Google",
         val repoList: List<Repo> = emptyList()
     )
+
+    sealed class Input {
+        object Submit : Input()
+        data class SetUserName(val userName: String) : Input()
+    }
 }
